eb4c55d08b6bb7764e956607c67372c5bfc69dda
jchan6532 <justinchan057@gmail.com>
2026-04-03 20:02:56 -0400

    implement main app, and importing document and query routers

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.api import documents, query
+
+app = FastAPI()
+
+app.include_router(documents.router)
+app.include_router(query.router)
+
+@app.get("/")
+def root():
+    return {"message": "Insightify Lite API running"}
