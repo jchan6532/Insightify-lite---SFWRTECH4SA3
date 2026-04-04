71257cac1d3cbf01520a58b6e7c24e95318d05b6
jchan6532 <justinchan057@gmail.com>
2026-04-03 20:00:45 -0400

    implement base db connection logic

---
+++ app/db.py
@@ -0,0 +1,16 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+def get_connection():
+    try:
+        conn = psycopg2.connect(DATABASE_URL)
+        return conn
+    except Exception as e:
+        print("Database connection failed:", e)
+        raise
