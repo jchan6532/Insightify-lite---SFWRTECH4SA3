7744d8840143e0481c9530312f7025567bd8ff90
jchan6532 <justinchan057@gmail.com>
2026-04-05 20:00:23 -0400

    update parser interface to support file bytes

--- app/parsers/base_parser.py
+++ app/parsers/base_parser.py
@@ -3,5 +3,5 @@ from abc import ABC, abstractmethod
 
 class BaseParser(ABC):
     @abstractmethod
-    def parse(self, raw_content: str) -> str:
+    def parse(self, raw_content: bytes) -> str:
         pass
